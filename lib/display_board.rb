ac7711eb4b809fe9b600af219052c83491a8bf52
Jenise Ruehle <jela07@gmail.com>
2019-12-28 19:34:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -73,3 +73,10 @@ end
 end  
 
 board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end 
