3e5928df04f813ef24e1a90caf89acce96d3165d
Jenise Ruehle <jela07@gmail.com>
2019-12-28 19:25:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -37,3 +37,10 @@ def display_board(board)
 end  
 
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end  
